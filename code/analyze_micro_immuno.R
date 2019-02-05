30002f16cc90b786f041b5a942a35de3088bafc1
akhagan <akhagan@umich.edu>
2019-02-05 18:38:28 -0500

    change dataset format, begin analysis

--- code/analyze_micro_immuno.R
+++ code/analyze_micro_immuno.R
@@ -1,4 +1,39 @@
 library(tidyverse)
+library(lubridate)
+library(hexbin)
 
-MI_data <- read_csv("data/micro_immuno/complete_dataset_14-19.csv")
+get_diversity_value <- function(x, y){
+  case_when(
+  x == "man" & y == "y" ~ paste("Cauc M"),
+  x == "man" & y == "n" ~ paste("POC M"),
+  x == "woman" & y == "y" ~ paste("Cauc W"),
+  x == "woman" & y == "n" ~ paste("POC W"))
+}
 
+MI_data <- read_csv("data/micro_immuno/complete_dataset_14-19.csv") %>% 
+  select(-Speaker, -Host) %>% distinct()
+
+MI_diversify <- MI_data %>% 
+  mutate(speaker_div = get_diversity_value(Speaker_gender, Speaker_caucasian)) %>% 
+  mutate(host_div = get_diversity_value(Host_gender, Host_caucasian))
+
+MI_diversify %>% 
+  ggplot()+
+  geom_bar(aes(x = year(Date), fill = as.factor(speaker_div)), position = "dodge")
+
+MI_diversify %>% 
+  filter(!is.na(host_div)) %>% 
+  filter(is.na(Lectureship)) %>% 
+  ggplot()+
+  geom_dotplot(aes(x = year(Date), fill = host_div), position = "jitter")
+  
+
+MI_data %>% 
+  filter(Role == "Speaker_gender") %>% 
+  group_by(year(Date), gender) %>% 
+  summarise(n = n())
+
+MI_data %>% 
+  filter(is.na(Lectureship)) %>% 
+  ggplot()+
+  geom_count(aes(x = gender, y = ))
--- code/compile_mi_datasets.R
+++ code/compile_mi_datasets.R
@@ -11,8 +11,8 @@ compiled_data <- raw_data %>% left_join(., host_data, by = "Host") %>%
   filter(!is.na(Speaker_gender)) %>% 
   mutate(Host_gender = ifelse(str_detect(Host, "Harry")==TRUE, "man", paste(Host_gender))) %>% 
   mutate(Host_caucasian = ifelse(str_detect(Host, "Harry")==TRUE, "y", paste(Host_caucasian))) %>% 
-  gather(Host_gender, Speaker_gender, key = "Role", value = "gender") %>% 
-  gather(Host_caucasian, Speaker_caucasian, key = "Role_c", value = "Caucasian") %>% 
+#  gather(Host_gender, Speaker_gender, key = "Role", value = "gender") %>% 
+#  gather(Host_caucasian, Speaker_caucasian, key = "Role_c", value = "Caucasian") %>% 
   distinct()
 
 write_csv(compiled_data, "data/micro_immuno/complete_dataset_14-19.csv")  
